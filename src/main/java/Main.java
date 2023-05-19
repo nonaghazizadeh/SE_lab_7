754536644bb966357744d356aed29f697802ebe7
re0078 <reza.abdollahzadeh12@gmail.com>
2023-05-19 23:37:32 +0330

    Refactor: Facade (for Main)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,13 +5,31 @@ import java.util.Scanner;
 import parser.Parser;
 
 public class Main {
+
+    private static ParserStarterFacade facade;
     public static void main(String[] args) {
-        Parser parser = new Parser();
         try {
-            // start parsing
-            parser.startParse(new Scanner(new File("src/main/resources/code")));
+            facade = new ParserStarterFacade("src/main/resources/code");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        facade.start();
+    }
+}
+
+class ParserStarterFacade {
+
+    private Parser parser;
+    private Scanner scanner;
+    private File file;
+
+    public ParserStarterFacade(String pathname) throws FileNotFoundException {
+        parser = new Parser();
+        file = new File(pathname);
+        scanner = new Scanner(file);
+    }
+
+    public void start(){
+        parser.startParse(scanner);
     }
 }
